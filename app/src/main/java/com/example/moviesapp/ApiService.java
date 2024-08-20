721e6589fe86782a53cb9a508d30daeeb76c3e57
stepann <stepan.snegovskoi@mail.ru>
2024-08-20 13:26:40 +0300

    Start create function loadWithFilters and FiltersActivity

---
+++ app/src/main/java/com/example/moviesapp/ApiService.java
@@ -0,0 +1,33 @@
+package com.example.moviesapp;
+
+import java.util.List;
+import java.util.Map;
+
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+
+public interface ApiService {
+
+    @GET("movie?token=B4SYYRK-VMHMMS2-G67AVP8-1JD6P9Z&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=30")
+    Single<MovieResponse> loadMovies(@Query("page") int page);
+
+    @GET("movie?token=B4SYYRK-VMHMMS2-G67AVP8-1JD6P9Z&field=id")
+    Single<TrailerResponse> loadTrailers(@Query("search") int id);
+
+    @GET("review?token=B4SYYRK-VMHMMS2-G67AVP8-1JD6P9Z&&field=movieId")
+    Single<ReviewResponse> loadReviews(@Query("search") int id);
+
+    @GET("movie?token=B4SYYRK-VMHMMS2-G67AVP8-1JD6P9Z")
+    Single<MovieResponse> loadMoviesWithFilters(
+            @Query("genre.name") List<String> options,
+            @Query("sortfield") String sortfield,
+            @Query("sorttype") int sorttype
+    );
+
+    // TODO МЕТОД ТОЛЬКОК НАПИСАН В СЕРВИСЕ, БОЛЬШЕ ЕГО
+    //  УПОМИНАНИЯ НИГДЕ НЕТУЦ, НЕОБХОДИМО ДОДЕЛАТЬ ВСЁ
+    //  НАДО ТАКЖЕ ДОДЕЛАТЬ ПАРАМЕТРЫ В МЕТОДЕ С ФИЛЬТРАМИ НА СТРОКЕК 22 В ЭТОМ КЛАССЕ
+
+}
